ab229ddc87ca6ebc1174f364378e069ccf3afd57
Michal Lauer <michal.lauer.25@gmail.com>
2026-02-07 21:21:46 +0100

    Added 0 party, factors

--- scripts/2silver/process_polls_and_parties.R
+++ scripts/2silver/process_polls_and_parties.R
@@ -5,6 +5,7 @@ library(tidyr)
 library(purrr)
 library(stringr)
 library(nanoparquet)
+library(forcats)
 
 # Prepare storage
 if (!dir.exists(config::get("path", config = "silver"))) {
@@ -18,14 +19,14 @@ df_polls <-
   # Sněmovní volby, 2025
   filter(volby == 166) |>
   unnest_wider(col = entries, names_repair = "unique_quiet") |>
-  select(
+  reframe(
     poll_id = id...1,
-    date_published = datum,
-    date_from = from,
-    date_to = to,
-    agency,
+    date_published = as.Date(datum),
+    date_from = as.Date(from),
+    date_to = as.Date(to),
+    agency = agency,
     sample_size = amount,
-    party,
+    party = party,
     voteshare = value,
     error
   ) |>
@@ -35,7 +36,7 @@ df_polls <-
   )
 
 # Clean errors
-df_polls_clean <-
+df_polls <-
   df_polls |>
   mutate(
     errors = if_else(
@@ -58,9 +59,42 @@ df_polls_clean <-
   ungroup() |>
   select(-c(error, errors_1, errors_2))
 
+# Compute party of "Others"
+df_polls <-
+  df_polls |>
+  group_by(poll_id) |>
+  group_modify(
+    \(x, group) {
+      current_sum <- sum(x$voteshare, na.rm = TRUE)
+      new_row <- x[1, ] |>
+        mutate(
+          party = 0,
+          voteshare = 1 - current_sum,
+          error_lower = NA,
+          error_upper = NA
+        )
+
+      bind_rows(x, new_row)
+    }
+  ) |>
+  ungroup() |>
+  arrange(-poll_id)
+
+# Create codes for parties and agencies
+df_polls <-
+  df_polls |>
+  mutate(
+    agency = factor(agency),
+    agency_id = as.integer(agency),
+    party = factor(party),
+    party_id = as.integer(party)
+  ) |>
+  relocate(agency_id, .after = agency) |>
+  relocate(party_id, .after = party)
+
 # Write polls
 write_parquet(
-  x = df_polls_clean,
+  x = df_polls,
   file = config::get("polls", config = "silver")
 )
 
@@ -70,7 +104,7 @@ df_parties <-
   map_df(\(x) {
     # Get primary data
     df <- tibble(
-      party_id = x[["VSTRANA"]],
+      party = x[["VSTRANA"]],
       party_shortcut = x[["ZKRATKA"]],
       party_name = x[["NAZEV"]]
     )
@@ -83,6 +117,19 @@ df_parties <-
 
     df
   })
+
+# Add party for Other and factorize
+df_parties <-
+  bind_rows(
+    tibble(
+      party = 0,
+      party_shortcut = "XXX",
+      party_name = "Other parties",
+      party_color = "#ed08d3ff"
+    ),
+    df_parties
+  ) |>
+  mutate(party = factor(party))
 
 # Write parties
 write_parquet(
