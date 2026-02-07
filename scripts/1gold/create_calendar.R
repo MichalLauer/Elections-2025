b4f743716bb1e5cd746705d5111acfbf8cecf152
Michal Lauer <michal.lauer.25@gmail.com>
2026-02-07 21:22:03 +0100

    Created calendar

--- scripts/1gold/create_calendar.R
+++ scripts/1gold/create_calendar.R
@@ -6,71 +6,30 @@ library(tidyr)
 library(tibble)
 
 # Read raw data
-df_raw <- read_parquet(config::get("data", config = "silver"))
-# Recode agencies
-df <-
-  df_raw |>
-  mutate(
-    party = as.character(party),
-    agency = recode_values(
-      agency,
-      from = names(mapping$agencies),
-      to = unlist(mapping$agencies)
-    )
-  )
+polls_raw <- read_parquet(config::get("polls", config = "silver"))
+parties_raw <- read_parquet(config::get("parties", config = "silver"))
 
-# Create raw calendar
-poll_day <- as.Date("2025-10-04")
-calendar_raw <-
-  tibble(
-    date = seq(min(df_raw$date), poll_day, by = "day")
-  ) |>
-  left_join(y = df, by = join_by(date), relationship = "one-to-many")
-
-# Create raw calendar with missing votes
-calendar_raw_wide <-
-  calendar_raw |>
-  select(date, agency, party, value) |>
-  pivot_wider(
-    names_from = "party",
-    values_from = "value"
-  ) |>
-  rowwise() |>
-  mutate(
-    "000" = 1 - sum(c_across(`768`:`1298`), na.rm = TRUE)
-  ) |>
-  ungroup()
-
-calendar_wide <-
-  calendar_raw |>
-  select(date, agency, from, to, duration_days, amount, attendance) |>
-  distinct() |>
-  left_join(
-    y = calendar_raw_wide,
-    by = join_by(date, agency),
-    relationship = "one-to-one"
-  )
-
+# Create calendar
+start_date <- min(polls_raw$date_published)
+end_date <- as.Date("2025-10-04")
 calendar_long <-
-  calendar_wide |>
-  pivot_longer(
-    cols = `768`:`000`,
-    names_to = "party",
-    values_to = "voteshare",
+  tibble(
+    date_published = seq(start_date, end_date, by = "day")
+  ) |>
+  mutate(
+    t = row_number()
   ) |>
   left_join(
-    distinct(df, party, name, shortcut, color),
-    by = join_by(party),
-    relationship = "many-to-one"
+    y = polls_raw,
+    by = join_by(date_published),
+    relationship = "one-to-many"
+  ) |>
+  left_join(
+    y = parties_raw,
+    by = join_by(party)
   )
-
-# Save calendar
-write_parquet(
-  x = calendar_wide,
-  file = config::get("calendar_wide", config = "gold")
-)
 
 write_parquet(
   x = calendar_long,
-  file = config::get("calendar_long", config = "gold"),
+  file = config::get("calendar_long", config = "gold")
 )
